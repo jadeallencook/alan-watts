5742b6307b503a46f3770114b4a330c5e577f2ec
24993473 <claytonleonardcook@gmail.com>
2020-01-22 18:34:58 -0500

    📄Updated Licensing Text

--- src/components/Licensing.tsx
+++ src/components/Licensing.tsx
@@ -4,32 +4,32 @@ import './Licensing.scss';
 const Licensing: React.FC = () => {
     return (
         <div className="Licensing">
-            <h2>Licensing For Commercial & Creative Use</h2>
-            <h3>Musical Use of Alan Watts Audio</h3>
-            <p>Use in a musical composition or music video to be posted on a streaming or online platform:</p>
+            <h3>Creative Film Use of Alan Watts Audio:</h3>
+            <h2>Licensing For Non-Profit, Educational and Inspirational Use:</h2>
             <ul>
-                <li>Online use considered promotional or commercial in nature requires a license. </li>
-                <li>Once licensed, artists may post their composition indefinitely; however, additional licensing is required if the piece is to be used in a film or music video, or as part of any commercial product, service, or brand promotion. </li>
-                <li>Please send a sample mix or your composition to licensing@alanwatts.com for review, and let us know if you are signed with a label, or are an independent artist.</li>
-                <li>Also please include the source (where you found the audio) as well as the length you would like to use, as licensing fees are calculated in 10-second increments.</li>
-                <li>We will be back in touch within two days with a quote or further questions. </li>
+                <li>Short filmmakers producing non-commercial educational or inspirational content may use one sample clip of up to 5 minutes in length.</li>
+                <li>The film must be original and not violate visual or music copyrights.</li>
+                <li>Maximum use is limited to three clips total per channel or platform, and additional clips or use in a full-length film or music video will be considered a commercial use.</li>
+                <li>Please send a copy to licensing@alanwatts.com for approval. Also, let us know if you would like your film to be considered for the Alan Watts Organization Channel Playlist.</li>
+                <li>The original talk title must be credited in the film and initially visible in posting information, with a link to the alanwatts.org website for curious viewers.</li>
+                <li>Student projects are generally considered inspirational or educational content upon review. Please send a final or rough cut to for approval.</li>
+                <li>Creative use audio may not be used for the promotion of any product or service, or for profit.</li>
             </ul>
-            <h3>Creative Film Use of Alan Watts Audio</h3>
-            <p>Licensing for non-profit, educational and inspirational use:</p>
+            <h3>Musical Use of Alan Watts Audio:</h3>
+            <p>Use in a musical composition or music video to be posted on streaming or online platforms:</p>
             <ul>
-                <li>Short film makers producing non-commercial educational or inspirational content may use one sample clip up to 5 minutes in length. The film must be original and not violate visual or music copyrights of any kind.</li>
-                <li>Maximum use is limited to three clips total per channel or platform. Additional clips will be considered commercial use. </li>
-                <li>Please send an initial copy or storyboard of your film to licensing@alanwatts.com for approval and let us know if you would like your film to be considered for the Alan Watts Organization YouTube Channel Playlist.</li>
-                <li>The original talk title must be credited in the film, and included in the online posting description or information box, along with a link to AlanWatts.org for curious viewers. </li>
-                <li>Student projects are generally considered inspirational or educational and will be assessed on a case by case basis. Please send a final or rough cut to licensing@alanwatts.com for approval.</li>
-                <li>Creative use audio may not be used to promote any product or service, or for profit. </li>
+                <li>Online use considered promotional and requires a use license.</li>
+                <li>Once licensed artists may post their composition indefinitely, however, additional licensing is required if the piece is to be used in a film or music video, or as part of a commercial product, service, or brand promotion.</li>
+                <li>Please send a copy or your composition to licensing@alanwatts.com for review.</li>
+                <li>Also please include the source (where you found the audio) as well as the length you would like to use as licensing fees are calculated for by ten seconds to be included, and let us know if you are signed with a label or are an independent artist.</li>
+                <li>We will be back in touch within two days with a quote or further questions.</li>
             </ul>
-            <h3>Commercial Licensing of Alan Watts Audio</h3>
-            <p>Licensing for commercial use or any use that promotes a company, product, or service:</p>
+            <h3>Commercial Licensing of Alan Watts Audio:</h3>
+            <p>Licensing for commercial use or any use that promotes a company, product, or service, or uses more content than provided for under Creative Use above:</p>
             <ul>
-                <li>Please submit a rough sample, storyboard or other proof of concept, including all brand associations and your contact information. </li>
-                <li>Outline the proposed length of the licensing term, the delivery platform(s), and source and length of the audio sample. (e.g., five years, Spotify, “Myth of Myself,” 2:10- 2:40)</li>
-                <li>Please send your proposal to licensing@alanwatts.com and we will respond with a quote or follow-up questions.</li>
+                <li>Please submit a rough sample, storyboard or other proof of concept, including all brand associations and your contact information.</li>
+                <li>Outline the proposed length of the licensing term, the delivery platform (s), and the source and length of the audio sample. (e.g. five years, Spotify, Myth of Myself, 2:10- 2:40)</li>
+                <li>Please send your proposal to licensing@alanwatts.com and we will respond with a quote or follow-up questions.</li>
             </ul>
         </div>
     );
