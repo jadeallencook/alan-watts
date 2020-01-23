2d67ac6a8c44d31bbdd6a6df50e9ed35b0ae7d55
Jade Allen Cook <jadeallencook@gmail.com>
2020-01-22 23:19:01 -0800

    🚀 Exported production build

---
+++ build/precache-manifest.a988d2bd8c3c0d379ecd99e755705f32.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8517f2bb8b40843372d62a92b45e4ad4",
+    "url": "/Alan-Watts/build/index.html"
+  },
+  {
+    "revision": "973d7176502c0812c588",
+    "url": "/Alan-Watts/build/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "2f90c381c691868d7e58",
+    "url": "/Alan-Watts/build/static/css/main.2343adc2.chunk.css"
+  },
+  {
+    "revision": "973d7176502c0812c588",
+    "url": "/Alan-Watts/build/static/js/2.42bc84fc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Alan-Watts/build/static/js/2.42bc84fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f90c381c691868d7e58",
+    "url": "/Alan-Watts/build/static/js/main.5d8dcd0f.chunk.js"
+  },
+  {
+    "revision": "bbe6ebb57a70c213fb21",
+    "url": "/Alan-Watts/build/static/js/runtime-main.be15e15c.js"
+  },
+  {
+    "revision": "13576137fbf0cd57f363de4c5e4c1573",
+    "url": "/Alan-Watts/build/static/media/alan-watts-boat-cropped.13576137.png"
+  },
+  {
+    "revision": "8fc433b6c58c92b4d01fc5bf3fda1653",
+    "url": "/Alan-Watts/build/static/media/mobile-boat-header.8fc433b6.png"
+  },
+  {
+    "revision": "243fe2487622409c9674ad0678d5ce12",
+    "url": "/Alan-Watts/build/static/media/ocean banner.243fe248.png"
+  },
+  {
+    "revision": "10bf673e36575baa540fe7a1cf766eae",
+    "url": "/Alan-Watts/build/static/media/white-logo.10bf673e.png"
+  }
+]);
